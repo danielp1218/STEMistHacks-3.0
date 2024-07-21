6d4233e91a55c34a20d0ec454a49a2615b1c495b
Skilled5041 <skilled5041@gmail.com>
2024-07-21 12:54:58 -0400

    changes

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,23 +1,24 @@
-import { error } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import {SECRET_API_KEY} from "$env/static/private";
+import type { RequestHandler } from "./$types";
+import { SECRET_API_KEY } from "$env/static/private";
 
 export const POST: RequestHandler = async ({ url }) => {
-	const wallet = (url.searchParams.get('wallet_address')) as string;
+	const wallet = (url.searchParams.get("wallet_address")) as string;
 
 	const formData = new FormData();
 
-	formData.append('allowPlatformToOperateToken', 'true');
-	formData.append('chain', 'sepolia');
-	formData.append('data', 'pet');
+	formData.append("allowPlatformToOperateToken", "true");
+	formData.append("chain", "sepolia");
+	formData.append("data", "pet");
 	formData.append("recipientAddress", wallet);
+	formData.append("imageUrl", "http://192.168.1.103/get_image")
 
-	const endpoint = 'https://api.verbwire.com/v1/nft/mint/quickMintFromMetadata';;
+	const endpoint = "https://api.verbwire.com/v1/nft/mint/quickMintFromMetadata";
+	;
 	const options = {
-		method: 'POST',
+		method: "POST",
 		headers: {
-			accept: 'application/json',
-			'X-API-Key': SECRET_API_KEY
+			accept: "application/json",
+			"X-API-Key": SECRET_API_KEY
 		}
 	};
 
@@ -27,7 +28,7 @@ export const POST: RequestHandler = async ({ url }) => {
 	})
 		.then(res => res.json())
 		.then(json => console.log(json))
-		.catch(err => console.error('error:' + err));
+		.catch(err => console.error("error:" + err));
 
-	return new Response()
+	return new Response();
 };
